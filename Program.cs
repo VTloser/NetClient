3fc9b8d20c157dd164f3564da3486fbbfab767b0
VTloser <39976734+VTloser@users.noreply.github.com>
2023-04-20 13:28:32 +0800

    NetClient创建
    NetClient创建

---
+++ Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class MainClass
+{
+    public static string Server_IP = "192.168.10.103";
+    public static int Server_Port = 10011;
+
+
+    public static string IP = "192.168.10.103";
+    static List<Socket> clientsockets = new List<Socket>();
+
+    public static void Main(string[] args)
+    {
+        for (int i = 20000; i < 20001; i++)
+        {
+            Thread thread = new Thread(SendMsg);
+            thread.IsBackground = true;
+            thread.Start(i);
+
+            Thread H = new Thread(SengHeat);
+            H.IsBackground = true;
+            H.Start(i + 200);
+
+            Thread Rece = new Thread(Receive);
+            Rece.IsBackground = true;
+            Rece.Start();
+
+
+            Thread Handle = new Thread(HandleEvent);
+            Handle.IsBackground = true;
+            Handle.Start();
+
+        }
+        Console.ReadLine();
+    }
+
+    private static void Receive()
+    {
+        while (true)
+        {
+            //用来保存发送发IP
+            EndPoint point = new IPEndPoint(IPAddress.Any, 0);
+            byte[] buff = new byte[1024 * 1024];
+
+            //foreach (var item in clientsockets)
+            //{
+            //    int length = item.ReceiveFrom(buff, ref point);
+            //    byte[] newbyte = new byte[length];
+            //    Array.Copy(buff, 0, newbyte, 0, length);
+
+
+            //    string msg = Encoding.UTF8.GetString(buff, 0, length);
+            //    Console.WriteLine("接收到来自:{ " + point.ToString() + " }的消息：[" + msg + "]");
+
+            //    ReceiveQueue.Enqueue(new ReceEvent(newbyte, point));
+            //}
+
+            if (clientsockets.Count > 0)
+            {
+                int length = clientsockets[0].ReceiveFrom(buff, ref point);
+                byte[] newbyte = new byte[length];
+                Array.Copy(buff, 0, newbyte, 0, length);
+
+
+                string msg = Encoding.UTF8.GetString(buff, 0, length);
+                Console.WriteLine("接收到来自:{ " + point.ToString() + " }的消息：[" + msg + "]");
+
+                ReceiveQueue.Enqueue(new ReceEvent(newbyte, point));
+            }
+
+            Thread.Sleep(0);
+        }
+    }
+
+
+    static Queue<ReceEvent> ReceiveQueue = new Queue<ReceEvent>();
+
+
+    private static void HandleEvent()
+    {
+        while (true)
+        {
+            if (ReceiveQueue.Count >= 1)
+            {
+                EndPoint endPoint;
+                MsgBase msgBase = Judge(out endPoint);
+                switch (msgBase)
+                {
+                    case MsgHeat:
+
+                        break;
+                    case MsgMove:
+                        Console.Write("位置移动X:" + ((MsgMove)msgBase).x + "!!!");
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            Thread.Sleep(0);
+        }
+    }
+
+    public static MsgBase Judge(out EndPoint endPoint)
+    {
+        if (ReceiveQueue.Count > 0)
+        {
+            ReceEvent temp = ReceiveQueue.Dequeue();
+            //拆包体
+            MsgBase msgBase = ProtocalTool.UnPackMessage(temp.buff);
+            endPoint = temp.SendClient;
+
+            if (msgBase is MsgHeat) return msgBase as MsgHeat;
+            else if (msgBase is MsgMove) return msgBase as MsgMove;  
+        }
+        endPoint = null;
+        return null;
+    }
+
+
+
+    private static void SengHeat(object? port)
+    {
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        clientsockets.Add(socket);
+
+        IPAddress ip = IPAddress.Parse(IP);
+        IPEndPoint endPoint = new IPEndPoint(ip, (int)port);
+        socket.Bind(endPoint);
+
+
+        IPEndPoint Serverip = new IPEndPoint(IPAddress.Parse(Server_IP), Server_Port);
+        //Console.WriteLine(Serverip);
+
+        int i = 0;
+        while (true)
+        {
+            Console.WriteLine($"发送次数{i++}");
+            MsgHeat msgMove = new MsgHeat();
+            socket.SendTo(msgMove.PackageMessage(), Serverip);
+            Thread.Sleep(1000);
+        }
+    }
+
+
+    private static void SendMsg(object? port)
+    {
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPAddress ip = IPAddress.Parse(IP);
+        IPEndPoint endPoint = new IPEndPoint(ip, (int)port);
+        socket.Bind(endPoint);
+
+
+        IPEndPoint Serverip = new IPEndPoint(IPAddress.Parse(Server_IP), Server_Port);
+        Console.WriteLine(Serverip);
+
+        int i = 0;
+        while (true)
+        {
+            Console.WriteLine($"发送次数{i++}");
+
+            MsgMove msgMove = new MsgMove();
+            msgMove.x = 10 + i;
+            msgMove.y = 20 + i;
+            msgMove.z = 30 + i;
+
+            socket.SendTo(msgMove.PackageMessage(), Serverip);
+
+            Thread.Sleep(2000);
+        }
+    }
+
+}
+
+public class SendInfo
+{
+    public MsgBase SendMsg;
+    public EndPoint SendClient;
+
+    public SendInfo(MsgBase sendMsg, EndPoint sendClient)
+    {
+        SendMsg = sendMsg;
+        SendClient = sendClient;
+    }
+}
+
+public class ReceEvent
+{
+    public byte[] buff;
+    public EndPoint SendClient;
+
+    public ReceEvent(byte[] buff, EndPoint sendClient)
+    {
+        this.buff = buff;
+        SendClient = sendClient;
+    }
+}
+
+
---
+++ Protocal.cs
@@ -0,0 +1,234 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+public class MsgBase
+{
+    public const int DataSize = sizeof(Int32);  //发送数据长度预设 4字节
+    public const int NameSize = sizeof(Int16);  //发送数据长度预设 2字节
+
+
+    public string protoName;
+
+    public SendType sendType = SendType.None;
+
+    public EndPoint SendClient; //发送客户端
+    //public ClientInfo[] Receiveclient; // TODO:特定广播 
+
+    /// <summary>
+    /// 编码
+    /// </summary>
+    /// <param name="msgBase"></param>
+    /// <returns></returns>
+    public static byte[] Encode(MsgBase msgBase)
+    {
+        string s = JsonConvert.SerializeObject(msgBase);
+        return Encoding.UTF8.GetBytes(s);
+    }
+
+    /// <summary>
+    /// 解码
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="bytes"></param>
+    /// <param name="offset"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static T Decode<T>(byte[] bytes, int offset, int count) where T : MsgBase
+    {
+        string s = Encoding.UTF8.GetString(bytes, offset, count);
+        MsgBase msgBase = (MsgBase)JsonConvert.DeserializeObject(s, Type.GetType("123"));
+        return (T)msgBase;
+    }
+
+
+    public static MsgBase Decode(Type type, byte[] bytes, int offset, int count)
+    {
+        string s = Encoding.UTF8.GetString(bytes, offset, count);
+        MsgBase msgBase = (MsgBase)JsonConvert.DeserializeObject(s, type);
+        return msgBase;
+    }
+
+
+
+
+    ///协议名的解码编码 使用Int16表示长度 (4 + 字符串)
+    public static byte[] EncodeName(MsgBase msgBase)
+    {
+        byte[] nameBytes = System.Text.Encoding.UTF8.GetBytes(msgBase.protoName);
+        //Int32 len = (Int32)nameBytes.Length;
+        Int16 len = (Int16)nameBytes.Length;
+        //申请bytes 数值
+        byte[] bytes = new byte[NameSize + len];
+
+        //组装2字节信息长度   2的8次方等于256 低位存放对应低位 高位存放对应高位 小段
+        bytes[0] = (byte)(len % 256);
+        bytes[1] = (byte)(len % 16777216 % 65536 / 256);
+        bytes[2] = (byte)(len % 16777216 / 65536);
+        bytes[3] = (byte)(len / 16777216);
+        ////组装替代方法
+        //byte[] length = BitConverter.GetBytes(len);
+
+        //组装名字
+        Array.Copy(nameBytes, 0, bytes, NameSize, len);
+        return bytes;
+    }
+
+
+    /// <summary>
+    /// 解码协议名
+    /// </summary>
+    /// <param name="bs"></param>
+    /// <param name="offset"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static string DecodeName(byte[] bs, int offset, out int count)
+    {
+        count = 0;
+        //最小长度应该大于2
+        if (offset + NameSize > bs.Length)
+        {
+            return "解析失败";
+        }
+
+        //读取长度
+        //Int32 len = (Int16)(bs[offset + 1] << 8 | bs[offset]);
+        ////读取长度替代方法
+        Int32 len = BitConverter.ToInt16(bs, offset);
+
+        if (len <= 0)
+        {
+            return "解析失败";
+        }
+        if (offset + NameSize + len > bs.Length)
+        {
+            return "解析失败";
+        }
+        //解析
+        count = NameSize + len;
+        string name = System.Text.Encoding.UTF8.GetString(bs, offset + NameSize, len);
+        return name;
+    }
+
+
+
+}
+
+public static class ProtocalTool
+{
+
+    public const int DataSize = sizeof(Int32);  //发送数据长度预设 4字节
+    public const int NameSize = sizeof(Int16);  //发送数据长度预设 2字节
+
+    /// <summary>
+    /// 数据封包
+    /// </summary>
+    /// <param name="msgBase"></param>
+    public static byte[] PackageMessage(this MsgBase msgBase)
+    {
+        //数据编码
+        byte[] nameBytes = MsgBase.EncodeName(msgBase);
+        byte[] bodyBytes = MsgBase.Encode(msgBase);
+
+        int len = nameBytes.Length + bodyBytes.Length;
+        byte[] SendBytes = new byte[DataSize + len];
+
+        //组装长度
+        SendBytes[0] = (byte)(len % 256);
+        SendBytes[1] = (byte)(len % 16777216 % 65536 / 256);
+        SendBytes[2] = (byte)(len % 16777216 / 65536);
+        SendBytes[3] = (byte)(len / 16777216);
+        //SendBytes = BitConverter.GetBytes(len); //大小端
+
+        //组装名字
+        Array.Copy(nameBytes, 0, SendBytes, DataSize, nameBytes.Length);
+        //组装消息体 
+        Array.Copy(bodyBytes, 0, SendBytes, DataSize + nameBytes.Length, bodyBytes.Length);
+
+        return SendBytes;
+    }
+
+
+    /// <summary>
+    /// 数据拆包
+    /// </summary>
+    /// <param name="dataBytes"></param>
+    /// <returns></returns>
+    public static MsgBase UnPackMessage(byte[] dataBytes)
+    {
+        Int16 bodyLength = BitConverter.ToInt16(dataBytes, 0);
+
+        int nameCount = 0;
+        string ProtoName = MsgBase.DecodeName(dataBytes, DataSize, out nameCount);
+
+        //Console.WriteLine(ProtoName);
+        //Console.WriteLine(nameCount);
+
+        if (ProtoName == "")
+        {
+            Console.WriteLine($"消息解析失败");
+            return null;
+        }
+
+        //解析协议体
+        MsgBase msgBase = MsgBase.Decode(Type.GetType(ProtoName), dataBytes, nameCount + DataSize, bodyLength - nameCount);
+
+        return msgBase;
+    }
+
+}
+
+[Serializable]
+public class MsgMove : MsgBase
+{
+    public MsgMove()
+    {
+        protoName = "MsgMove";
+        sendType = SendType.Other;
+    }
+
+    public int x = 0;
+    public int y = 0;
+    public int z = 0;
+
+}
+
+[Serializable]
+public class MsgHeat : MsgBase
+{
+    public MsgHeat()
+    {
+        protoName = "MsgHeat";
+        sendType = SendType.None;
+    }
+}
+
+
+public struct SyncState
+{
+    public float Pox_x;
+    public float Pox_y;
+    public float Pox_z;
+
+    public float Rot_x;
+    public float Rot_y;
+    public float Rot_Z;
+
+}
+
+public enum SendType
+{
+    None,      //不发送
+    Broadcast, //广播
+    Self,      //自己
+    Other,     //除了自己之外
+    Specific,  //特播
+
+}
